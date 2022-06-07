160de0310523db08e12fa5ebc386825a041a1240
Daniel Burke <danielseanburke@gmail.com>
2022-06-07 15:04:40 -0230

    Open file and print contents line-by-line

--- rvint.cpp
+++ rvint.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 
+enum class TokenType
+{
+    Newline,
+    Comment,
+    Symbol
+};
+
+struct Token
+{
+    TokenType type;
+    std::string token;
+};
+
+void parse_input(std::ifstream& file)
+{
+    std::string line;
+
+    while (std::getline(file, line)) 
+    {
+        std::cout << line << std::endl;
+    }
+}
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello, world!\n";
+    if (argc < 2) { return 1; }
+
+    std::ifstream file(argv[1]);
+
+    if (!file.is_open()) { return 1; }
+
+    parse_input(file);
+
     return 0;
 }
