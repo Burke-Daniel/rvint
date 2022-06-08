7267073261725d64b20227ca23799e24388ad61a
Daniel Burke <danielseanburke@gmail.com>
2022-06-08 13:39:44 -0230

    use strtok instead

--- rvint.cpp
+++ rvint.cpp
@@ -1,20 +1,46 @@
+#include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <vector>
 
-enum class TokenType
-{
-    Newline,
-    Comment,
-    Symbol
-};
+#include <stdio.h>
+#include <string.h>
 
-struct Token
+
+// Lexer reads token char-by-char to see what type of token it will be
+// Basically should remove comments from being parsed
+
+std::vector<std::string> tokens;
+
+void print_tokens()
 {
-    TokenType type;
-    std::string token;
-};
+    std::cout << "Tokens: { ";
+    for (const auto& token : tokens) { std::cout << token << " "; }
+    std::cout << "}\n";
+}
+
+bool has_char(const std::string& s, char c)
+{
+    return s.find(c) != std::string::npos;
+}
+
+void remove_from_string(std::string& s, char c)
+{
+    s.erase(std::remove(s.begin(), s.end(), c), s.end());
+}
+
+void tokenize(char* line)
+{
+    char* curr_token;
+    curr_token = strtok(line, " ,:()");
+    while (curr_token != nullptr)
+    {
+        tokens.push_back(curr_token);
+        curr_token = strtok(nullptr, " ,:()");
+    }
+}
 
 void parse_input(std::ifstream& file)
 {
@@ -22,8 +48,11 @@ void parse_input(std::ifstream& file)
 
     while (std::getline(file, line)) 
     {
-        std::cout << line << std::endl;
+        char tokenizable_line[512];
+        strcpy(tokenizable_line, line.c_str());
+        tokenize(tokenizable_line);
     }
+    print_tokens();
 }
 
 int main(int argc, char* argv[])
